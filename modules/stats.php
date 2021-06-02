bdae44766f5581e4747441991bd47c3e051e5ba9
Adam Sewell <awsewell@pm.me>
2021-06-01 21:20:49 -0400

    added logging and adjusted verification

--- modules/stats.php
+++ modules/stats.php
@@ -7,15 +7,18 @@
 
     public static function add_stats($data){
       $options = get_option('tinyshield_options');
+      self::write_log('tinyShield: stats initiated - ' . $data->ip_address);
 
       if(empty($options['tinyshield_stats'])){
         if($data->action == 'allow'){
+          self::write_log('tinyShield: stats blank - creating allowed ' . $data->ip_address);
           $options['tinyshield_stats'] = serialize(
             array('allowed' => array(
               strtotime('today') => 1,
             )
           ));
         }elseif($data->action == 'block'){
+          self::write_log('tinyShield: stats blank - creating blocked ' . $data->ip_address);
           $options['tinyshield_stats'] = serialize(
             array('blocked' => array(
               strtotime('today') => 1,
@@ -27,10 +30,12 @@
       }else{
         $stats = unserialize($options['tinyshield_stats']);
 
-        if(is_array($stats)){
+        if(is_array($stats) && is_object($data)){
           if($data->action == 'allow'){
+            self::write_log('tinyShield: adding one point to allowed stats ' . $data->ip_address);
             $stats['allowed'][strtotime('today')]++;
           }elseif($data->action == 'block'){
+            self::write_log('tinyShield: adding one point to blocked stats ' . $data->ip_address);
             $stats['blocked'][strtotime('today')]++;
           }
         }
